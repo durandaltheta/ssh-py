ec716575b2c32e12ecca55008bf865d0c80e1b6e
durandaltheta <blaynetdennis@gmail.com>
2017-12-30 00:45:09 -0500

    made install script build and install compact bytecode archives

--- install.sh
+++ install.sh
@@ -7,10 +7,15 @@ else
     SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     mkdir -p /usr/local/bin 
 
+    # build self contained archives
+    $SCRIPT_DIR/make.sh
+
     # copy files
     cp $SCRIPT_DIR/src/ssh-py /usr/local/bin 
-    cp $SCRIPT_DIR/src/ssh.py /usr/local/bin 
+    cp $SCRIPT_DIR/build/pyssh /usr/local/bin 
     cp $SCRIPT_DIR/src/ssh-py3 /usr/local/bin 
-    cp $SCRIPT_DIR/src/ssh3.py /usr/local/bin 
-    cp -r $SCRIPT_DIR/src/paramiko /usr/local/bin
+    cp $SCRIPT_DIR/build/pyssh3 /usr/local/bin 
+
+    # clean directories
+    $SCRIPT_DIR/clean.sh
 fi
